3a40bb553a6ff77bfce534bb42c7431ade4ca678
sagrvma <notsagarverma@gmail.com>
2025-03-17 22:26:08 +0530

    css updated for res-card

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ const RestaurantCard = ({ resData }) => {
           resData.info.cloudinaryImageId
         }
       ></img>
-      <h3>{name}</h3>
+      <h2>{name}</h2>
       <h4>{cuisines.join(", ")}</h4>
       <h4>{avgRating}</h4>
       <h4>{costForTwo}</h4>
