08a57de83d734172245c73cf70b5802a682b9136
sagrvma <notsagarverma@gmail.com>
2025-05-17 17:24:31 +0530

    Added github user info api to user card in about page

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,6 @@ const About = () => {
     <div className="about-body">
       <h1>About Us</h1>
       <User name="Sagar" location="Delhi" contact="@sagrvma"></User>
-      <UserClass name="Sagar" location="Delhi" contact="@sagrvma"></UserClass>
     </div>
   );
 };
--- src/components/User.js
+++ src/components/User.js
@@ -1,9 +1,30 @@
-const User = ({ name, location, contact }) => {
+import { useEffect, useState } from "react";
+
+const User = () => {
+  const [name, setName] = useState("Dummy Name");
+  const [location, setLocation] = useState("Dummy Location");
+  const [username, setUsername] = useState("dummy-user");
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const response = await fetch("https://api.github.com/users/sagrvma");
+    const data = await response.json();
+
+    // console.log(data);
+
+    setName(data.name);
+    setLocation(data.location);
+    setUsername(data.login);
+  };
+
   return (
     <div className="user-card">
       <h2>Name: {name}</h2>
       <h3>Location: {location}</h3>
-      <h3>Contact: {contact}</h3>
+      <h3>Contact: {username}</h3>
     </div>
   );
 };
